01b5765c0bf577332f44653ac15a3290455a26d9
Ali Khalid <46325569+AIMI-SAYO@users.noreply.github.com>
2022-08-20 12:50:41 -0700

    new tools, better formating

--- CustomRenderPanel.py
+++ CustomRenderPanel.py
@@ -10,14 +10,26 @@ class CustomRenderPanel(bpy.types.Panel):
     
     def draw(self, context):
         layout = self.layout
-        ##########################################
+        
+        
+        row = layout.row()
         self.layout.label(text='Render Settings')
+        layout.operator("render.render", text = "Engine Render")
+        layout.operator("render.opengl", text = "Viewport Render")
+        layout.operator("")
         
-        layout.operator("render.render")
         
-        ##########################################
+        row = layout.row()
         self.layout.label(text='Camera Settings')
-        layout.operator("view3d.camera_to_view")
-        layout.operator("view3d.camera_to_view_selected")
+        layout.operator("view3d.view_camera", text = "View Camera")
+        layout.operator("view3d.camera_to_view", text = "Camera to View")
+        layout.operator("view3d.camera_to_view_selected", text = "Camera Fit Selected")
+        layout.operator("view3d.walk", text = "Free Camera")
+        
+        
+        #### TEST ####
+        row = layout.row()
+        self.layout.label(text='!!!! TEST !!!!')
+        layout.operator("")
 
 bpy.utils.register_class(CustomRenderPanel)
